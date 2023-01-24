a58c6800d62b3d537fe3f7b12cc8e1f046605cf6
Dirk Burgers <dirk.burgers@humanoids.nl>
2023-01-24 12:01:37 +0100

    Add mobile header

---
+++ general.js
@@ -0,0 +1,17 @@
+const primaryNav = document.querySelector(".main-nav");
+const navToggle = document.querySelector(".mob-nav-toggle");
+
+navToggle.addEventListener("click", () => {
+const visibility = primaryNav.getAttribute("data-visible");
+
+if (visibility === "false") {
+    primaryNav.setAttribute("data-visible", true);
+    navToggle.setAttribute("aria-expanded", true);
+    navToggle.classList.add("open");
+} else if (visibility === "true") {
+    primaryNav.setAttribute("data-visible", false);
+    navToggle.setAttribute("aria-expanded", false);
+    navToggle.classList.remove("open");
+}
+
+});
