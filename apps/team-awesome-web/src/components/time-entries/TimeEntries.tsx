c31932c51ef41f841dba0ab044c199d6a87da20c
Dirk Burgers <dirk.burgers@humanoids.nl>
2023-02-15 12:04:44 +0100

    Add add this time entry with rest

--- apps/team-awesome-web/src/components/time-entries/TimeEntries.tsx
+++ apps/team-awesome-web/src/components/time-entries/TimeEntries.tsx
@@ -1,51 +1,13 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import React, { useState, useEffect } from "react";
-
 import { TimeEntry } from "../time-entry";
 import * as Types from "../../types";
-import { NotFoundError } from "../../services/errors";
 
-export const TimeEntries = () => {
-  const [timeEntries, setTimeEntries] = useState<Types.TimeEntry[]>([]);
-  const [errorMessage, setErrorMessage] = useState("");
+interface TimeEntriesProps {
+  timeEntries: Types.TimeEntry[];
+}
 
-  const getTimeEntries = async (): Promise<Types.TimeEntry[]> => {
-    return fetch("http://localhost:3004/time-entries", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((response) => {
-        if (response.status === 404) {
-          throw new NotFoundError();
-        }
-        console.log(response.status);
-        return response;
-      })
-      .then((response) => response.json())
-      .catch((error) => error);
-  };
-
-  const fetchTimeEntries = async () => {
-    const fetchedTimeEntries = await getTimeEntries();
-    if (fetchedTimeEntries instanceof NotFoundError) {
-      console.log("not found");
-
-      setErrorMessage("Time entries were not found");
-      return;
-    }
-    setTimeEntries(fetchedTimeEntries);
-  };
-
-  useEffect(() => {
-    fetchTimeEntries();
-  }, []);
-
+export const TimeEntries = ({ timeEntries }: TimeEntriesProps) => {
   return (
     <>
-      {errorMessage ? <p>{errorMessage}</p> : null}
-
       {timeEntries.map((timeEntry) => (
         <>
           <TimeEntry
