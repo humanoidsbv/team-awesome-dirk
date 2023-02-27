f5272a21f4b369bb28a7d499934a80c76e358fd3
Dirk Burgers <dirk.burgers@humanoids.nl>
2023-02-27 15:14:55 +0100

    Sort time-entries feature

--- apps/team-awesome-web/src/components/time-entries/TimeEntries.tsx
+++ apps/team-awesome-web/src/components/time-entries/TimeEntries.tsx
@@ -1,14 +1,42 @@
+import { useState, useContext, ChangeEvent } from "react";
+import { StoreContext } from "../store-context";
 import { TimeEntry } from "../time-entry";
 import * as Types from "../../types";
 
 interface TimeEntriesProps {
-  timeEntries: Types.TimeEntry[];
   handleDeleteEntry: (input: Types.TimeEntry) => void;
 }
 
-export const TimeEntries = ({ timeEntries, handleDeleteEntry }: TimeEntriesProps) => {
+export const TimeEntries = ({ handleDeleteEntry }: TimeEntriesProps) => {
+  const [sortOrder, setSortOrder] = useState("ascending");
+  const { timeEntries, setTimeEntries } = useContext(StoreContext);
+
+  const sortOptions = ["ascending", "descending"];
+
+  const handleSort = ({ target }: ChangeEvent<HTMLSelectElement>) => {
+    const selectSortOrder = target.value;
+
+    const sorted = [...timeEntries].sort((a, b) => {
+      if (a.id < b.id) {
+        return selectSortOrder === "ascending" ? -1 : 1;
+      }
+      if (a.id > b.id) {
+        return selectSortOrder === "ascending" ? 1 : -1;
+      }
+      return 0;
+    });
+    setSortOrder(sortOrder);
+    setTimeEntries(sorted);
+  };
+
   return (
     <>
+      <label htmlFor="sort-time-entries">Sort time entries</label>
+      <select onChange={handleSort} name="dropdown" id="sort-time-entries">
+        <option value="">--Please choose an option--</option>
+        <option value={sortOptions[0]}>Ascending</option>
+        <option value={sortOptions[1]}>Descending</option>
+      </select>
       {timeEntries.map((timeEntry) => (
         <TimeEntry key={timeEntry.id} handleDeleteEntry={handleDeleteEntry} timeEntry={timeEntry} />
       ))}
