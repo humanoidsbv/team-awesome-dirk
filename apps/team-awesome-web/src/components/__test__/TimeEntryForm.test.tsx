039302f2ab84f9c7ccbb5ff9689b4573a4ee827a
Dirk Burgers <dirk.burgers@humanoids.nl>
2023-03-02 14:55:50 +0100

    Add teammember test

---
+++ apps/team-awesome-web/src/components/__test__/TimeEntryForm.test.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { render, screen } from "@testing-library/react";
+import { TeamMembers } from "../team-members";
+// import { TimeEntryForm } from "../Form/time-entry-form";
+
+it("renders a team member", () => {
+  const mockMembers = [
+    {
+      id: 1,
+      firstName: "Herman",
+      lastName: "Vulkers",
+      emailAddress: "hermanvulkers@gmail.com",
+      label: "test",
+      currentClient: "Humanoids",
+    },
+  ];
+
+  const teamMembers = render(<TeamMembers memberEntries={mockMembers} />);
+
+  expect(teamMembers.findByTestId("team-member"));
+  // expect(herman).toBeInTheDocument;
+});
